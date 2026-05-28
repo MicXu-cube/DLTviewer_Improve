62f7999250542b8dc4c1b9f885a7f6e2d7863390
MicXu-cube <WuBin19967@gmail.com>
2026-05-28 11:33:35 +0800

    Add files via upload

---
+++ src/version.h
@@ -0,0 +1,35 @@
+/**
+ * @licence app begin@
+ * Copyright (C) 2011-2012  BMW AG
+ *
+ * This file is part of GENIVI Project Dlt Viewer.
+ *
+ * Contributions are licensed to the GENIVI Alliance under one or more
+ * Contribution License Agreements.
+ *
+ * \copyright
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a  copy of the MPL was not distributed with
+ * this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \file version.h
+ * For further information see http://www.genivi.org/.
+ * @licence end@
+ */
+
+#ifndef __VERSION_H_
+#define __VERSION_H_
+/* version change recommendation */
+/* changing minor & major when layout of settings file config.ini changes */
+/* this kind of change is tracked in the settings dialogr */
+/* for other bugfixes and not major feature enhancement just use patch level */
+#define PACKAGE_VERSION_STATE "1.0 Alpha"
+#define PACKAGE_MAJOR_VERSION 2
+#define PACKAGE_MINOR_VERSION 23
+#define PACKAGE_PATCH_LEVEL 0
+#define PACKAGE_VERSION "2.23.0"
+#define PACKAGE_REVISION ""
+#define PACKAGE_DESCRIPTION "DLT Viewer"
+#define DLT_SUPPORT_MAIL_ADDRESS "WuBin@163.com"
+#define DLT_SUPPORT_NAME "GENIVI X Neusoft"
+#endif
